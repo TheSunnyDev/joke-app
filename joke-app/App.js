dbdd756134b9fe3443a153852d962e950ad1ecbf
TheSunnyDev <109054640+TheSunnyDev@users.noreply.github.com>
2022-12-26 09:38:22 -0800

    Add project files
    First files for the project.

---
+++ joke-app/App.js
@@ -0,0 +1,26 @@
+import React from "react"
+import Joke from "./Joke"
+
+export default function App(){
+    return(
+        <div>
+        <h1>🤣 Jokes for Days 🤣</h1>
+            <Joke 
+                setup="I got my daughter a fridge for her birthday."
+                punchline="I can't wait to see her face light up when she opens it." />
+               
+            <Joke 
+                setup="How did the hacker escape the police?"
+                punchline="He just ransomware!" />
+            
+            <Joke 
+                setup="Why don't pirates travel on mountain roads?"
+                punchline="Scurvy." />
+                
+            <Joke 
+                setup="Why do bees stay in the hive in the winter?"
+                punchline="Swarm." />
+            
+        </div>
+    )
+}
---
+++ joke-app/README.md
@@ -0,0 +1,4 @@
+--About TheSunnyDev - Joke App
+A light joke app using React.
+
+Happy for any and all feedback/support for future versions 💖!
